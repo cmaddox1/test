e88da9b40e1547d0750d194c745e4442016a39a4
cmaddox1 <cmaddox1@babson.edu>
2016-09-03 16:03:41 -0400

    Third Commit

--- calc.py
+++ calc.py
@@ -1,3 +1,14 @@
 print( 46 + 37 + 38)
 print(2016-1995)
 print(2**6)
+
+print('Seconds in 42min 42sec')
+print((42*60)+42)
+
+print('miles in 10kil')
+print(10/1.61)
+
+print('Average Pace per mile')
+print((42+(42/60))/(10/1.61))
+print('Average Speed')
+print((42+(42/60))/(60/(10/1.61))
